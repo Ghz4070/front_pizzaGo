2bf92b17731a68c68044cd6d25da96678e7dfa43
Raphael <paiva.raphaelt@gmail.com>
2020-06-24 18:36:22 +0200

    create navigation

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-     <h1>hello header</h1>
+  <div class="d-flex flex-wrap flex-row justify-space-between align-center">
+    <img src="~/static/PizzaGo_final.png" height="150" width="150">
+    <nav class="navigation">
+        <nuxt-link to="">Connexion</nuxt-link>
+        <nuxt-link to="">Inscription</nuxt-link>
+        <nuxt-link to="">Commander</nuxt-link>
+        <nuxt-link to="">Contact</nuxt-link>
+    </nav>
   </div>
 </template>
 
@@ -11,5 +17,15 @@ export default {
 </script>
 
 <style>
+
+    .navigation a {
+        padding: 10px 10px;
+        margin: 0px 10px;
+        text-decoration: none;
+        color: black;
+        border: 1px solid #979797;
+        border-radius: 10px;
+        background-color: white; 
+    }
 
 </style>
