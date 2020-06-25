93f6387778236872f02e1462b038f64aa3fe3b3b
Raphael <paiva.raphaelt@gmail.com>
2020-06-25 16:40:43 +0200

    add props className

--- components/Table_Cart.vue
+++ components/Table_Cart.vue
@@ -1,22 +1,25 @@
 <template>
-  <table>
-      <thead>
-          <tr>
-              <td v-for="(title, index) in Thead" :key="index">{{title}}</td>
-          </tr>
-      </thead>
-      <tbody>
-          <tr>
-              <td>test</td>
-          </tr>
-      </tbody>
-  </table>
+    <div :class="className">
+        <table>
+            <thead>
+                <tr>
+                    <td v-for="(title, index) in Thead" :key="index">{{title}}</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>test</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
 export default {
     props: {
-        Thead: Array
+        Thead: Array,
+        className: String
     },
 }
 </script>
