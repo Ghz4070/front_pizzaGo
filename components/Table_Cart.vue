289b5abf7424ecb688169fb57304ad06051f8c58
Raphael <paiva.raphaelt@gmail.com>
2020-06-25 15:59:14 +0200

    creation table cart

---
+++ components/Table_Cart.vue
@@ -0,0 +1,26 @@
+<template>
+  <table>
+      <thead>
+          <tr v-for="(title, index) in THead" :key="index">
+              <td>{{title}}</td>
+          </tr>
+      </thead>
+      <tbody>
+          <tr>
+              <td>test</td>
+          </tr>
+      </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+    props: {
+        Thead: Array
+    },
+}
+</script>
+
+<style>
+
+</style>
