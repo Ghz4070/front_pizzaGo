a0ee6f4a5e9e5d89914b08f740e496e73d5fb5af
Raphael <paiva.raphaelt@gmail.com>
2020-06-25 02:55:52 +0200

    add condition to display logo if a token is here

--- components/Header.vue
+++ components/Header.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="d-flex flex-wrap flex-row justify-space-between align-center">
     <img src="~/static/PizzaGo_final.png" height="150" width="150">
-    <nav class="navigation">
-        <nuxt-link to="">Connexion</nuxt-link>
-        <nuxt-link to="">Inscription</nuxt-link>
-        <nuxt-link to="">Commander</nuxt-link>
-        <nuxt-link to="">Contact</nuxt-link>
+    <nav class="navigation d-flex flex-row flex-wrap align-center">
+        <template v-if="img">
+            <nuxt-link to="">Commander</nuxt-link>
+            <nuxt-link to="">Contact</nuxt-link>
+            <img src="~/static/user.svg" height="30" width="30">
+        </template>
+        <template v-else>
+            <nuxt-link to="">Connexion</nuxt-link>
+            <nuxt-link to="">Inscription</nuxt-link>
+            <nuxt-link to="">Commander</nuxt-link>
+            <nuxt-link to="">Contact</nuxt-link>
+        </template>
     </nav>
   </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            img : ''
+        }
+    },
+    mounted() {
+        if(localStorage.key('x-access-token')){
+            this.img = true
+        }else {
+            this.img = false
+        }
+    },
 }
 </script>
 
