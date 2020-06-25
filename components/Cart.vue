6e9f4031089f7e8c471dcd0bfc9074b9968345a1
Raphael <paiva.raphaelt@gmail.com>
2020-06-25 15:58:50 +0200

    Create cart component

---
+++ components/Cart.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="d-flex flex-column flex-wrap justify-end test .rounded-xl">
+      <h1>Panier</h1>
+      <div>
+          <form>
+            <label for="promo">Code promo</label>
+            <input type="text">
+            <button>Valider</button>
+          </form>
+      </div>
+      <div>
+          <table>
+            <thead>
+                <tr>
+                    <th>Pizza</th>
+                    <th>Taille</th>
+                    <th>Quantité</th>
+                    <th>Prix</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>name</td>
+                    <td>taille</td>
+                    <td>Quantité</td>
+                    <td>prix</td>
+                </tr>
+            </tbody>
+          </table>
+      </div>
+      <div>
+         <TableCart :thead="tableBoisson" />
+      </div>
+      <div>
+         
+      </div>
+      <div>
+          <p>Total :</p>
+          <p>le montant</p>
+      </div>
+      <a href="">Commander</a>
+  </div>
+</template>
+
+<script>
+import TableCart from './Table_Cart';
+
+export default {
+    components: {
+        TableCart
+    },
+    data() {
+        return {
+            tableBoisson : ['Boissons', 'Quantité', 'Prix'],
+            tableDessert : ['Desserts', 'Quantité', 'Prix']
+        }
+    }
+}
+</script>
+
+<style>
+    .test {
+        height: 100%;
+        width: 23%;
+        border: 3px solid black
+    }
+</style>
