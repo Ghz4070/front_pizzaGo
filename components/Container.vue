c3585c03c67e6571cffe8ea6ff1445e498f3ebe0
Alex <alex.rigueur@gmail.com>
2020-06-25 15:32:09 +0200

    set tabs with dynamic display

--- components/Container.vue
+++ components/Container.vue
@@ -2,37 +2,77 @@
   <div>
     <v-card class="mx-auto" outlined>
       <v-container>
-        <v-row no-gutters>
+
+        <v-tabs class="tabs" @change="changeTab(currentTab)" centered v-model="currentTab" background-color="white" color="black">
+          <v-tab  v-for="item in items" :key="item.tab">{{ item.tab }}</v-tab>
+        </v-tabs>
+
+        <!-- PIZZA -->
+        <v-row v-if="currentTab == 0" no-gutters>
           <v-col cols="12" sm="12" v-for="category in categories" :key="category.id">
             <div class="category">Catégorie : {{ category.name }}</div>
             <v-col v-for="(pizza, index) in pizzas" :key="pizza.id" cols="12" sm="4">
-              <v-card v-if="category.name == pizza.category.name"
-                outlined class="ma-3 pa-3 card_pizza" max-width="400">
-                <v-img height="200px" class="white--text align-end"
+              <v-card
+                v-if="category.name == pizza.category.name"
+                outlined
+                class="ma-3 pa-3 card_pizza"
+                max-width="400"
+              >
+                <v-img
+                  height="200px"
+                  class="white--text align-end"
                   src="https://camionapizzaangouleme.files.wordpress.com/2018/02/pizza-free-png-image.png?w=1200"
                 ></v-img>
 
                 <v-card-subtitle class="pb-0 pizza_name">{{ pizza.name }}</v-card-subtitle>
 
                 <v-card-text class="text--primary center">
-                  <v-select :change="updatePrice(index,pizza.quantity, pizza.price, pizza.size)" v-model="pizza.size" :items="size" label="Taille"></v-select>
+                  <v-select
+                    :change="updatePrice(index,pizza.quantity, pizza.price, pizza.size)"
+                    v-model="pizza.size"
+                    :items="size"
+                    label="Taille"
+                  ></v-select>
                   <v-row>
                     <v-col cols="12" sm="6" md="6">
-                      <v-text-field @change="updatePrice(index,pizza.quantity, pizza.price, pizza.size)" v-model="pizza.quantity" type="number" label="x1" value="1" solo></v-text-field>
+                      <v-text-field
+                        @change="updatePrice(index,pizza.quantity, pizza.price, pizza.size)"
+                        v-model="pizza.quantity"
+                        type="number"
+                        label="x1"
+                        value="1"
+                        solo
+                      ></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="6">
-                      <v-text-field disabled label="" :value="pizza.price+ ' €'" solo></v-text-field>
+                      <v-text-field disabled label :value="pizza.price+ ' €'" solo></v-text-field>
                     </v-col>
                   </v-row>
                 </v-card-text>
 
                 <v-card-actions class="center">
-                  <v-btn v-on:click="addToCart(pizza)" small class="ma-2" outlined color="indigo">Ajouter</v-btn>
+                  <v-btn
+                    v-on:click="addToCart(pizza)"
+                    small
+                    class="ma-2"
+                    outlined
+                    color="indigo"
+                  >Ajouter</v-btn>
                 </v-card-actions>
               </v-card>
             </v-col>
           </v-col>
         </v-row>
+
+        <!-- Boisson -->
+        <div v-if="currentTab == 1" class="">
+          Boisson work !
+        </div>
+
+        <!-- Ingredient -->
+        <div v-if="currentTab == 2">
+          Ingredient Work !
+        </div>
       </v-container>
     </v-card>
   </div>
@@ -43,10 +83,16 @@ import axios from "axios";
 export default {
   data: function() {
     return {
+      currentTab: 0,
       pizzas: [],
       categories: null,
       size: ["S", "M", "L", "XL"],
-      add: {quantity: 1, size: 'M', price:'10'}
+      add: { quantity: 1, size: "M", price: "10" },
+      items: [
+        { tab: "Pizza", content: "1" },
+        { tab: "Boisson", content: "2" },
+        { tab: "Dessert", content: "3" }
+      ]
     };
   },
   async mounted() {
@@ -58,26 +104,26 @@ export default {
     getPizzas() {
       return axios
         .get(`http://localhost:4000/api/v1/pizza`)
-        .then((res) => {
+        .then(res => {
           this.pizzas = res.data.result;
         })
         .catch(e => {
           console.log("catch");
         });
     },
-    updatePrice(i,nb,price,size) {
+    updatePrice(i, nb, price, size) {
       let new_price;
       switch (size) {
-        case 'S':
+        case "S":
           new_price = nb * 8;
           break;
-        case 'M':
+        case "M":
           new_price = nb * 10;
           break;
-        case 'L':
+        case "L":
           new_price = nb * 12;
           break;
-        case 'XL':
+        case "XL":
           new_price = nb * 15;
           break;
         default:
@@ -89,7 +135,7 @@ export default {
       this.pizzas = [];
       pizzas.forEach(element => {
         let el = Object.assign(element, this.add);
-        this.pizzas = [...this.pizzas, el]
+        this.pizzas = [...this.pizzas, el];
       });
     },
     getCategories() {
@@ -102,6 +148,10 @@ export default {
           console.log("catch");
         });
     },
+    changeTab(val) {
+      this.currentTab = val;
+      console.log(val);
+    },
     addToCart(pizza) {
       console.log(pizza);
     }
@@ -113,6 +163,9 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
 div {
   font-family: "Pacifico", cursive;
+}
+.v-sheet.v-card {
+  border-radius: 30px;
 }
 .center {
   text-align: center;
@@ -128,13 +181,16 @@ div {
   text-align: center;
   font-size: 20px;
 }
-.v-card__text{
+.v-card__text {
   padding: 0 16px;
 }
 .v-card__actions {
-    padding: 0;
+  padding: 0;
 }
 .card_pizza {
   border: 0 !important;
+}
+.tabs {
+  padding-bottom: 30px;
 }
 </style>
