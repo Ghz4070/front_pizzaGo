a5c716e8f9561e51aa362ab71f5f6acd75852f26
Raphael <paiva.raphaelt@gmail.com>
2020-06-28 18:14:15 +0200

    create section add ingrediant, html, method

--- components/Table_Cart.vue
+++ components/Table_Cart.vue
@@ -13,6 +13,67 @@
                             <td :key="element.name">{{element.name}}</td>
                             <td :key="element.size">{{element.size}}</td>
                             <td :key="element.price">{{element.price}} €</td>
+                            <td @click="getId(key)">Modifier</td>
+                            <v-dialog
+                                v-model="dialog"
+                                max-width="390"
+                            >
+                                <v-card class="pt-3 pr-3 pb-3 pl-3">
+                                    <v-card-title class="headline">Ingrédiant</v-card-title>
+                                    <div class="d-flex flex-row flex-wrap" id="sauce">
+                                        <v-select
+                                            v-model="currentSelect.sauce"
+                                            label="Sauces" 
+                                            :items="ingredients.ingredients.sauces.items"
+                                        />
+                                        <button @click="addIngrediant">Ajouter</button>
+                                    </div>
+                                    <div class="d-flex flex-row flex-wrap" id="viande">
+                                        <v-select
+                                            v-model="currentSelect.viande"
+                                            label="Viandes" 
+                                            :items="ingredients.ingredients.viandes.items"
+                                        />
+                                        <button @click="addIngrediant">Ajouter</button>
+                                    </div>
+                                    <div class="d-flex flex-row flex-wrap" id="legume">
+                                        <v-select
+                                            v-model="currentSelect.legume"
+                                            label="Légumes" 
+                                            :items="ingredients.ingredients.legumes.items"
+                                        />
+                                        <button @click="addIngrediant">Ajouter</button>
+                                    </div>
+                                    <div class="d-flex flex-row flex-wrap" id="fromage">
+                                        <v-select
+                                            v-model="currentSelect.fromage"
+                                            label="Fromages" 
+                                            :items="ingredients.ingredients.fromages.items"
+                                        />
+                                        <button @click="addIngrediant">Ajouter</button>
+                                    </div>
+                                    <div class="d-flex flex-row flex-wrap" id="epice">
+                                        <v-select
+                                            v-model="currentSelect.epice"
+                                            label="Epices" 
+                                            :items="ingredients.ingredients.epices.items"
+                                        />
+                                        <button @click="addIngrediant">Ajouter</button>
+                                    </div>
+
+                                    <v-card-actions>
+                                    <v-spacer></v-spacer>
+
+                                    <v-btn
+                                        color="green darken-1"
+                                        text
+                                        @click="getAddIngrediant(key)"
+                                    >
+                                        Valider
+                                    </v-btn>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
                         </tr>
                     </template>
                 </template>
@@ -42,7 +103,24 @@
 export default {
     data() {
         return {
-            totalPricePizza : null
+            totalPricePizza : null,
+            dialog: false,
+            currentSelect : {
+                id: '',
+                sauce : '',
+                viande: '',
+                legume: '',
+                fromage: '',
+                epice: ''
+            },
+            ingrediantSelected: {
+                id:'',
+                sauce : [],
+                viande: [],
+                legume: [],
+                fromage: [],
+                epice: []
+            }
         }
     },
     watch: {
@@ -85,6 +163,42 @@ export default {
                 dessertTotal = dessertTotal + element.price;
             }
             this.$emit("totalDessert", dessertTotal);
+        },
+        getId(e){
+            this.currentSelect.id = e;
+            this.dialog = true;
+        }
+        ,
+        addIngrediant(e){
+            if(!this.ingrediantSelected.id) this.ingrediantSelected.id = this.currentSelect.id;
+            switch (e.path[1].id) {
+                case 'sauce':
+                    this.ingrediantSelected.sauce.push(this.currentSelect.sauce)
+                    break;
+                case 'viande':
+                    this.ingrediantSelected.viande.push(this.currentSelect.viande)
+                    break;
+                case 'legume':
+                    this.ingrediantSelected.legume.push(this.currentSelect.legume)
+                    break;
+                case 'fromage':
+                    this.ingrediantSelected.fromage.push(this.currentSelect.fromage)
+                    break;
+                case 'epice':
+                    this.ingrediantSelected.epice.push(this.currentSelect.epice)
+                    break;
+                default:
+                    break;
+            }
+        },
+        getAddIngrediant() {
+            this.$emit("ingrediantAdded",this.ingrediantSelected);
+            this.dialog = false;
+        }
+    },
+    computed: {
+        ingredients() {
+            return this.$store.state.ingredients
         }
     }
 }
