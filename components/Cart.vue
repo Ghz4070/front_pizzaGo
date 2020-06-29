401850a88eddd49fcbb9715ae5b47f55182631c4
Raphael <paiva.raphaelt@gmail.com>
2020-06-29 17:44:28 +0200

    display total ingrediant

--- components/Cart.vue
+++ components/Cart.vue
@@ -33,14 +33,18 @@
         class="d-flex flex-row flex-wrap justify-space-around max-width"
       />
       <div class="d-flex flex-row flex-wrap justify-space-around max-width">
+        <h3>Ingrédients ajoutés</h3>
+        <p>{{cart.total}} €</p>
+      </div>
+      <div class="d-flex flex-row flex-wrap justify-space-around max-width">
         <p>Total :</p>
         <template v-if="promo">
           <p>{{promo}}%</p>
-          <p>{{(totalPrice.total - ( totalPrice.total * (promo/100) ))}} €</p>
-          <p>{{totalPrice.total}} €</p>
+          <p>{{(totalPrice.total - ( totalPrice.total * (promo/100) ) + cart.total)}} €</p>
+          <p>{{totalPrice.total + cart.total}} €</p>
         </template>
         <template v-else>
-          <p>{{totalPrice.total}}€</p>
+          <p>{{totalPrice.total + cart.total}}€</p>
         </template>
       </div>
 
@@ -50,7 +54,7 @@
             class="buy-button center"
             v-bind="attrs"
             v-on="on"
-          >Paiement - {{ promo ? (totalPrice.total - ( totalPrice.total * (promo/100) )) : totalPrice.total }} €</a>
+          >Paiement - {{ promo ? (totalPrice.total - ( totalPrice.total * (promo/100) ) + cart.total) : totalPrice.total + cart.total}} €</a>
         </template>
         <v-card>
           <v-card-title class="headline">Paiement de votre commande</v-card-title>
@@ -100,7 +104,7 @@ export default {
       tableBoisson: ["Boissons", "Prix"],
       tableDessert: ["Desserts", "Prix"],
       promo: null,
-      cart: {},
+      cart: {content: {}, total: 0},
       totalPrice: { pizza: 0, drink: 0, dessert: 0, ingrediant: 0, total: 0 },
       paiement: false,
       publishableKey: "pk_test_QrgGFFIn2rjHnwgwvakXU0dn00FhK9IbmE",
@@ -212,14 +216,13 @@ export default {
       console.log(`la promo est ${total}`);
       this.cart.contents.pizzas[id].ingrediantAdded = ingrediantObject;
       const newJson = {
-        contents: this.cart.contents
-      };
-      const totalJSON = {
+        contents: this.cart.contents,
         total: total
       };
+
       const JSONtostring = JSON.stringify(newJson);
       localStorage.setItem("datas", JSONtostring);
-      localStorage.setItem("total", total);
+      
     },
     ingrediantRemove(ingrediantRemove) {
       const { id } = ingrediantRemove;
