1aa96a66d96cbae0eb2a03fa483391de554b1a35
Ghz4070 <ghezal-ilies@outlook.fr>
2020-06-29 22:38:59 +0200

    diplay users

--- components/admin/pizza/Pizza.vue
+++ components/admin/pizza/Pizza.vue
@@ -3,39 +3,55 @@
     <v-row>
       <AddPizza />
     </v-row>
-    <v-row dense>
-      <v-col v-for="(pizza, index) in pizzas" :key="index" cols="12" sm="12" md="4">
-        <div v-if="loading" class="center">
-          <v-skeleton-loader
-            class="mt-6"
-            max-width="400"
-            elevation="6"
-            type="card"
-            transition="scale-transition"
-          ></v-skeleton-loader>
-        </div>
-        <div v-else>
-          <v-card outlined class="ma-3 pa-3 card_pizza" elevation="6" max-width="400">
-            <v-img
-              :src="pizza.img"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
+    <v-row>
+      <v-col
+        v-for="(pizza, index) in pizzas"
+        :key="index"
+        cols="12"
+        xl="3"
+        lg="4"
+        md="6"
+        sm="12"
+        align="center"
+      >
+        <v-skeleton-loader
+          v-if="loading"
+          class="mt-6"
+          max-width="400"
+          elevation="6"
+          type="card"
+          transition="scale-transition"
+        ></v-skeleton-loader>
+
+        <v-hover v-else>
+          <template v-slot="{ hover }">
+            <v-card
+              outlined
+              class="ma-3 pa-3 card_pizza transition-swing"
+              :class="`elevation-${hover ? 24 : 6}`"
+              max-width="400"
             >
-              <v-card-title v-text="pizza.name"></v-card-title>
-            </v-img>
+              <v-img
+                :src="pizza.img"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                height="200px"
+              >
+                <v-card-title v-text="pizza.name"></v-card-title>
+              </v-img>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
+              <v-card-actions>
+                <v-spacer></v-spacer>
 
-              <EditPizza :dataPizza="pizza" />
+                <EditPizza :dataPizza="pizza" />
 
-              <v-btn icon class="ml-3">
-                <v-icon>mdi-trash-can</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </div>
+                <v-btn icon class="ml-3">
+                  <v-icon>mdi-trash-can</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-hover>
       </v-col>
     </v-row>
   </v-container>
