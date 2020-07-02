fc84a9177547e14710cb8d10da43586542ce0eff
Ghz4070 <ghezal-ilies@outlook.fr>
2020-07-02 23:51:57 +0200

    fixed display order client with admin

--- components/admin/order/EditOrder.vue
+++ components/admin/order/EditOrder.vue
@@ -26,8 +26,66 @@
         <v-expansion-panel-content>
           <v-container>
             <v-row>
-              <v-col cols="12" xl="12">
-                <span>Pizza : {{ dataOrders.content }}</span>
+              <v-col
+                v-for="(pizza, index) in dataOrders.content.contents.pizzas"
+                :key="index"
+                cols="12"
+                xl="6"
+              >
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-hover>
+                      <template v-slot="{ hover }">
+                        <v-card
+                          v-bind="attrs"
+                          v-on="on"
+                          outlined
+                          class="ma-3 pa-3 card_pizza transition-swing"
+                          :class="`elevation-${hover ? 24 : 6}`"
+                          max-width="400px"
+                        >
+                          <v-img
+                            :src="pizza.img"
+                            class="white--text align-end"
+                            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                            height="100px"
+                          >
+                            <v-card-title v-text="pizza.name"></v-card-title>
+                          </v-img>
+                        </v-card>
+                      </template>
+                    </v-hover>
+                  </template>
+                  Sauce :
+                  <span
+                    v-for="compo in pizza.composition.sauces.items"
+                    :key="compo.label"
+                  >{{ compo + " " }}</span>
+                  <br />Fromages :
+                  <span
+                    v-for="compo in pizza.composition.fromages.items"
+                    :key="compo.label"
+                  >{{ compo + " " }}</span>
+                  <br />Viandes :
+                  <span
+                    v-for="compo in pizza.composition.viandes.items"
+                    :key="compo.label"
+                  >{{ compo + " " }}</span>
+                  <br />Legumes :
+                  <span
+                    v-for="compo in pizza.composition.legumes.items"
+                    :key="compo.label"
+                  >{{ compo + " " }}</span>
+                  <br />Epices :
+                  <span
+                    v-for="compo in pizza.composition.epices.items"
+                    :key="compo.label"
+                  >{{ compo + " " }}</span>
+                </v-tooltip>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                </v-card-actions>
               </v-col>
               <v-col cols="12" xl="12">
                 <span>Prix : {{ dataOrders.price | euroSign }}</span>
@@ -83,36 +141,36 @@ export default {
     }
   },
   methods: {
-    async saveOrder() {
+    saveOrder() {
       this.getDatas();
-      try {
-        const response = await this.$axios.put(
-          `https://server-api-pizzago.herokuapp.com/api/v1/admin/order/update`,
-          this.params,
-          {
-            headers: {
-              "x-access-token": localStorage.getItem("x-access-token")
-            }
+      this.$axios
+        .put(`http://localhost:4000/api/v1/admin/order/update`, this.params, {
+          headers: {
+            "x-access-token": localStorage.getItem("x-access-token")
           }
-        );
-        // this.$emit("updateOrder", response.data.result);
-        this.params.status = this.status[response.data.result.status - 1].label;
-      } catch (error) {
-        console.log(error);
-      }
+        })
+        .then(res => {
+          // https://server-api-pizzago.herokuapp.com/
+          // console.log(res.data.result);
+          this.params.status = this.status[res.data.result.status - 1].label;
+          // console.log(this.params.status);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     },
     getDatas() {
       // item peux directement aller chercher l'index
       this.statusId = this.status.find(
         item => item.label == this.params.status
       ).id;
+
       this.params = {
         id: this.dataOrders.id,
         price: this.dataOrders.price,
         status: this.statusId,
         userId: this.dataOrders.user.id,
-        content: this.dataOrders.content,
-        promoId: this.dataOrders.promo.id
+        content: this.dataOrders.content
       };
     }
   },
